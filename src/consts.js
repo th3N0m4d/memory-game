82540f3a799540658dcbb077994553080f49ec1a
th3N0m4d <edielton.dantas@hotmail.com>
2020-02-08 10:29:10 -0300

    Create levels for rendering the Board

---
+++ src/consts.js
@@ -0,0 +1,17 @@
+export const levels = {
+  easy: {
+    rows: 3,
+    cols: 4,
+    label: 'Easy'
+  },
+  medium: {
+    rows: 4,
+    cols: 5,
+    label: 'Medium'
+  },
+  hard: {
+    rows: 6,
+    cols: 6,
+    label: 'Hard'
+  }
+}
