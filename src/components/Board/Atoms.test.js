cc2cf3b44b67ca561bae590599f56bdd7b2ad1c6
th3N0m4d <edielton.dantas@hotmail.com>
2020-02-09 08:46:41 -0300

    Add test coverage for GridContainer

---
+++ src/components/Board/Atoms.test.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { GridContainer } from './Atoms'
+
+describe('Board Atoms', () => {
+  it('should render', () => {
+    const wrapper = shallow(
+      <GridContainer />
+    )
+
+    expect(wrapper.exists()).toBeTruthy()
+  })
+
+  it('should update the grid\'s rows dimensions', () => {
+    const rows = 3
+
+    const wrapper = shallow(
+      <GridContainer rows={rows} />
+    )
+
+    expect(wrapper).toHaveStyleRule('grid-template-rows', 'repeat(3,100px)')
+  })
+
+  it('should update the grid\'s columns dimensions', () => {
+    const cols = 4
+
+    const wrapper = shallow(
+      <GridContainer cols={cols} />
+    )
+
+    expect(wrapper).toHaveStyleRule('grid-template-columns', 'repeat(4,1fr)')
+  })
+
+})
