1c2bd3bdf73b4308e45bc6f05e7726c711da62a8
th3N0m4d <edielton.dantas@hotmail.com>
2020-02-12 11:21:36 -0300

    Increase height of cards from 100px to 130px

--- src/components/Board/Atoms.jsx
+++ src/components/Board/Atoms.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const GridContainer = styled.div`
     display: grid;
     grid-template-columns: repeat(${({ cols }) => cols}, 1fr);
-    grid-template-rows: repeat(${({ rows }) => rows}, 100px);
+    grid-template-rows: repeat(${({ rows }) => rows}, 130px);
     row-gap: 4px;
     column-gap: 4px;
 `
